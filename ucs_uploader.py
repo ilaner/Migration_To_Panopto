3e3fb17d1d7af571b2217785962f645ee4934e0b
ilane <ilanerukh@gmail.com>
2020-10-15 09:19:09 +0300

    fixed bugs

--- ucs_uploader.py
+++ ucs_uploader.py
@@ -100,6 +100,7 @@ class UcsUploader:
         # step 4 - finish the upload
         self.__finish_upload(session_upload)
         self.__setup_or_refresh_access_token()
+        time.sleep(5)
         return session_upload['SessionId']
         # step 5 - monitor the progress of processing
         # self.__monitor_progress(upload_id)
